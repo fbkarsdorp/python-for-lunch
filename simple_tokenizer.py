3b5220a09cbe5e577599df9e1763e2e9524713c4
Folgert Karsdorp <fbkarsdorp@gmail.com>
2015-09-28 17:43:31 +0200

    finished data carpentry

---
+++ simple_tokenizer.py
@@ -0,0 +1,52 @@
+# code taken from gensim's util model, see: https://github.com/piskvorky/gensim/blob/develop/gensim/utils.py
+
+import re
+import sys
+import unicodedata
+from six import u
+
+if sys.version_info[0] >= 3:
+    unicode = str
+
+PAT_ALPHABETIC = re.compile('(((?![\d])\w)+)', re.UNICODE)
+RE_HTML_ENTITY = re.compile(r'&(#?)([xX]?)(\w{1,8});', re.UNICODE)
+
+def any2unicode(text, encoding='utf8', errors='strict'):
+    """Convert a string (bytestring in `encoding` or unicode), to unicode."""
+    if isinstance(text, unicode):
+        return text
+    return unicode(text, encoding, errors=errors)
+to_unicode = any2unicode
+
+def deaccent(text):
+    """
+    Remove accentuation from the given string. Input text is either a unicode string or utf8 encoded bytestring.
+    Return input string with accents removed, as unicode.
+    >>> deaccent("Šéf chomutovských komunistů dostal poštou bílý prášek")
+    u'Sef chomutovskych komunistu dostal postou bily prasek'
+    """
+    if not isinstance(text, unicode):
+        # assume utf8 for byte strings, use default (strict) error handling
+        text = text.decode('utf8')
+    norm = unicodedata.normalize("NFD", text)
+    result = u('').join(ch for ch in norm if unicodedata.category(ch) != 'Mn')
+    return unicodedata.normalize("NFC", result)
+
+def tokenize(text, lowercase=False, deacc=False, errors="strict", to_lower=False, lower=False):
+    """
+    Iteratively yield tokens as unicode strings, removing accent marks
+    and optionally lowercasing the unidoce string by assigning True
+    to one of the parameters, lowercase, to_lower, or lower.
+    Input text may be either unicode or utf8-encoded byte string.
+    The tokens on output are maximal contiguous sequences of alphabetic
+    characters (no digits!).
+    >>> list(tokenize('Nic nemůže letět rychlostí vyšší, než 300 tisíc kilometrů za sekundu!', deacc = True))
+    [u'Nic', u'nemuze', u'letet', u'rychlosti', u'vyssi', u'nez', u'tisic', u'kilometru', u'za', u'sekundu']
+    """
+    lowercase = lowercase or to_lower or lower
+    text = to_unicode(text, errors=errors)
+    if lowercase:
+        text = text.lower()
+    if deacc:
+        text = deaccent(text)
+    return [match.group() for match in PAT_ALPHABETIC.finditer(text)]
